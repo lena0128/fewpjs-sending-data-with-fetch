8325527b890b77f30082f12204a964444a4db8dc
Lena Hou <lenahou2013@gmail.com>
2021-09-26 15:08:50 -0400

    commit tree 9d7b3957e6571d18b39a9316eafae7d1418e7c99

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Add your code here
+
+
+function submitData(name, email) {
+
+    let configObj = {
+        method: "post",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify({name, email})
+    };
+
+    return fetch("http://localhost:3000/users", configObj)
+    .then(response => response.json())
+    .then(function(object){
+        document.body.innerHTML = object["id"]
+    })
+    .catch(function(error){
+        document.body.innerHTML = error.message
+    })
+}
+
